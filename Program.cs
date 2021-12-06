19c00ffb8312c1ec3b29cb4b698fa64b7b19e79b
drehcris <88004033+drehcris@users.noreply.github.com>
2021-12-06 16:17:22 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,69 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Cafeteria
 {
-    class Program : Menu
+    class Program
     {
         static void Main(string[] args)
         {
-            string opcao;
-            Menu menu = new Menu();
-            do
-            {
-                opcao = menu.menu();
-                switch (opcao)
+                string opcao;
+                Menu menu = new Menu();
+                MySql091121 mys = new MySql091121();
+                do
                 {
-                    case "1":
-                        Console.WriteLine("Café");
-                        Cafe cafe = new Cafe();
-                        cafe.cadastrarCafe();
-                        cafe.cadastrarBebida();                      
+                    opcao = menu.menu();
+                    switch (opcao)
+                    {
+                        case "1":
+                            Console.WriteLine("Café");
+                            Cafe cafe = new Cafe();
+                            cafe.cadastrarCafe();
+                            cafe.cadastrarBebida();
+                            mys.chec();
+                           
                         break;
-                    case "2":
-                        Console.WriteLine("Chá");
-                        Chas chas = new Chas();
-                        chas.cadastrarChas();
-                        chas.cadastrarBebida();
+                        case "2":
+                            Console.WriteLine("Chá");
+                            Chas chas = new Chas();
+                            chas.cadastrarChas();
+                            chas.cadastrarBebida();
+                            mys.chec();
                         break;
-                    case "3":
-                        Console.WriteLine("Chocolates");
-                        Chocolates chocolates = new Chocolates();
-                        chocolates.cadastrarChocolates();
-                        chocolates.cadastrarBebida();
+                        case "3":
+                            Console.WriteLine("Chocolates");
+                            Chocolates chocolates = new Chocolates();
+                            chocolates.cadastrarChocolates();
+                            chocolates.cadastrarBebida();
+                            mys.chec();
                         break;
-                    case "4":
-                        Console.WriteLine("Clássicos");
-                        Classicos classicos = new Classicos();
-                        classicos.cadastrarClassicos();
-                        classicos.cadastrarBebida();
+                        case "4":
+                            Console.WriteLine("Clássicos");
+                            Classicos classicos = new Classicos();
+                            classicos.cadastrarClassicos();
+                            classicos.cadastrarBebida();
+                            mys.chec();
                         break;
-                    case "5":
-                        Console.WriteLine("Frappuccinos");
-                        Frappuccinos frappuccinos = new Frappuccinos();
-                        frappuccinos.cadastrarFrappuccinos();
-                        frappuccinos.cadastrarBebida();
+                        case "5":
+                            Console.WriteLine("Frappuccinos");
+                            Frappuccinos frappuccinos = new Frappuccinos();
+                            frappuccinos.cadastrarFrappuccinos();
+                            frappuccinos.cadastrarBebida();
+                            mys.chec();
                         break;
-                    case "6":
-                        Console.WriteLine("Doces");
-                        Doces doces = new Doces();
-                        doces.cadastrarDoces();
-                        doces.cadastrarComida();
+                        case "6":
+                            Console.WriteLine("Doces");
+                            Doces doces = new Doces();
+                            doces.cadastrarDoces();
+                            doces.cadastrarComida();
+                            mys.chec();
                         break;
-                    case "7":
-                        Console.WriteLine("Salgados");
-                        Salgados salgados = new Salgados();
-                        salgados.cadastrarSalgados();
-                        salgados.cadastrarComida();
+                        case "7":
+                            Console.WriteLine("Salgados");
+                            Salgados salgados = new Salgados();
+                            salgados.cadastrarSalgados();
+                            salgados.cadastrarComida();
+                            mys.chec();
                         break;
+
                     case "S":
-                        Console.WriteLine("Encerrando o programa");
-                        break;
-                    default:
-                        Console.WriteLine("Opção inválida!");
-                        break;
-                }
-            } while (opcao != "S");
+                            Console.WriteLine("Encerrando o programa");
+                            break;
+                        default:
+                            Console.WriteLine("Opção inválida!");
+                            break;
+                    }
+                } while (opcao != "S");
             //Environment.Exit(0);
         }
+      }
     }
-}
