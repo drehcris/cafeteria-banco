94b939e87f7aef82dc6e3c7df7b210ab8d942f54
drehcris <88004033+drehcris@users.noreply.github.com>
2021-12-06 16:26:07 -0300

    Add files via upload

--- Chas.cs
+++ Chas.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Cafeteria
 {
-    class Chas : Bebida
+    class Chas : Bebidas
     {
         public void cadastrarChas()
         {
@@ -21,6 +23,8 @@ namespace Cafeteria
 
             string opcao;
             opcao = Console.ReadLine();
+            Console.WriteLine("Voce escolheu {0}", opcao);
         }
     }
 }
+
